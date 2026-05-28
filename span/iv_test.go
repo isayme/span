7375d49564f5ac97e7d3cff7f5b6f100eff0c8dd
isayme <isaymeorg@gmail.com>
2026-05-28 17:08:35 +0800

    handle test

--- span/iv_test.go
+++ span/iv_test.go
@@ -1,6 +1,7 @@
 package span
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,8 +10,28 @@ import (
 func TestGenIv(t *testing.T) {
 	require := require.New(t)
 
-	buf := make([]byte, 16)
-	genIV(258, buf)
-	require.Equal(byte(2), buf[15])
-	require.Equal(byte(1), buf[14])
+	testCases := []struct {
+		pos int64
+		iv  []byte
+	}{
+		{0, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+		{1, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+		{15, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+		{16, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16}},
+		{17, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16}},
+		{31, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16}},
+		{32, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32}},
+		{33, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32}},
+		{255, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 240}},
+		{256, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0}},
+		{257, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("pos: %d", tc.pos), func(t *testing.T) {
+			buf := mustRandomBytes(16)
+			genIV(tc.pos, buf)
+			require.Equal(tc.iv, buf)
+		})
+	}
 }
